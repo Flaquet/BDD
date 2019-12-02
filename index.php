c50e2aef62572b4c437126f460c3c4bccc901907
FLAQUET <55395047+Flaquet@users.noreply.github.com>
2019-12-02 16:41:01 +0100

    commit

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
-<?php require("user.php");?>
+<?php require("user.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    
-        $User1 = new User();
-        $User1->setNom("Edouard");
-        $User1->setMdp("123");
-        $User1->AfficherNom();
-        $User1->AfficherMdp();
 
-    ?>    
+    $User1 = new User();
+    $User1->setNom("Edouard");
+    $User1->setMdp("123");
+    $User1->AfficherNom();
+    $User1->AfficherMdp();
+
+    ?>
 
     <form method="POST">
         <h1>Connexion</h1>
@@ -27,22 +29,41 @@
         <label><b>Mot de passe</b></label>
         <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-        <input type="submit" id='submit' value='LOGIN' >
+        <input type="submit" id='submit' value='LOGIN'>
 
     </form>
 
     <?php
 
-        if (isset($_POST['username'])) {
-            $isConnect = $User1->verifMdp($_POST['username'],$_POST['password']);
-            if($isConnect){
-                echo "connecté";
-            }else{
-                echo "erreur";
-            }
+    if (isset($_POST['username'])) {
+        $isConnect = $User1->verifMdp($_POST['username'], $_POST['password']);
+        if ($isConnect) {
+            echo "connecté";
+        } else {
+            echo "erreur";
         }
+
+        try{
+            $BaseFlaquet=new PDO('mysql:host=192.168.65.204; dbname=Docteur; charset=utf8','FlaquetFlemal', 'FlaquetFlemal');
+            echo "select * from medicament" ;
+            $ResultRequet = $BaseFlaquet->query("select * from medicament"); 
+            echo "<table border = 1> <tr><td>id medicament</td><td>medicament</td></tr>";
+            while ($Table = $ResultRequet->fetch()){
+                 echo "<tr> ";
+                 echo '<td> '.$Table["codeSS"].' </td><td> '.$Table["nomcommer"].'</td>' ;
+                 echo "<tr>";
+            }
+            $ResultRequet->closeCursor();
+            echo "</table>";
+
+        }
+         catch (Exception $erreur){
+             echo $erreur->getMessage();
+        }
+    }    
 
     ?>
 
 </body>
+
 </html>
--- user.php
+++ user.php
@@ -1,48 +1,34 @@
 <?php
 
-    class User {
+class User
+{
 
-        private $_username;
-        private $_password;
+    private $_username;
+    private $_password;
 
-        public function AfficherNom(){
-
-            echo "<p> Le nom est ".$this->_username."</p>";
-
-        }
-
-        public function AfficherMdp(){
-
-            echo "<p> Le MDP est ".$this->_password."</p>";
-
-        }
-
-        public function setMdp($password){
-
-            $this->_password = $password;
-
-        }
-
-
-        public function setNom($NouveauNom){
-
-            $this->_username = $NouveauNom;
-
-        }
-        public function verifMdp($usrname, $pwd){
-
-            if($usrname == $this->_username){
-                if($pwd == $this->_password){
-
-                    return true;
-
-                }
-
-            }
-            return false;
-
-        }
-
+    public function AfficherNom()
+    {
+        echo "<p> Le nom est " . $this->_username . "</p>";
     }
-
-?>
+    public function AfficherMdp()
+    {
+        echo "<p> Le MDP est " . $this->_password . "</p>";
+    }
+    public function setMdp($password)
+    {
+        $this->_password = $password;
+    }
+    public function setNom($NouveauNom)
+    {
+        $this->_username = $NouveauNom;
+    }
+    public function verifMdp($usrname, $pwd)
+    {
+        if ($usrname == $this->_username) {
+            if ($pwd == $this->_password) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
