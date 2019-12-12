08547571e5ed966093d2758173bfd0dc11a09a01
FLAQUET <55395047+Flaquet@users.noreply.github.com>
2019-12-12 10:16:42 +0100

    Commit

--- ExerciceTwo/index.php
+++ ExerciceTwo/index.php
@@ -1,13 +1,73 @@
 <?php require("personnage.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <h1>Exercice 2</h1>
+
+    <?php
+
+    $Jouer1 = new Personnage("Julien", "");
+    $Jouer1->affiche();
+    $Jouer2 = new Personnage("Edouard", "");
+    $Jouer2->affiche();
+
+    ?>
+
+    <textarea name="" id="" cols="70" rows="30">
+    Index
+    <php
+
+    $Jouer1 = new Personnage("Julien", "");
+    $Jouer1->affiche();
+    $Jouer2 = new Personnage("Edouard", "");
+    $Jouer2->affiche();
+ 
+    ?>
+    Personnage
+    <php
+
+    class Personnage{
+
+    private $Pseudo;
+    private $Vie;
+
+    public function __construct($pseudo, $vie){
+
+        $this->Pseudo = $pseudo;
+        $vie = "100";
+        $this->Vie = $vie;
+       
+
+    }
+
+    public function affiche(){
+
+        ?>  <span class="br"> 
+            <php 
+        echo    "Le Joueur : ".$this->Pseudo." a  : ".$this->Vie." de Vie."; 
+        ?> 
+    </span> 
+    <php
+        
+
+    }
+
+}
+
+    ?>
+
+    </textarea>
+
+
 </body>
+
 </html>
--- ExerciceTwo/personnage.php
+++ ExerciceTwo/personnage.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="style.css">
 <?php
 
 class Personnage{
@@ -7,7 +8,17 @@ class Personnage{
 
     public function __construct($pseudo, $vie){
 
-        echo
+        $this->Pseudo = $pseudo;
+        $vie = "100";
+        $this->Vie = $vie;
+       
+
+    }
+
+    public function affiche(){
+
+        ?>  <span class="br"> <?php echo    "Le Joueur : ".$this->Pseudo." a  : ".$this->Vie." de Vie."; ?> </span> <?php
+        
 
     }
 
